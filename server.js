49bdeb78a7139cef03901eff3a9fe42edf0f56cf
Lexi127 <aperalta127@gmail.com>
2019-11-16 12:53:25 -0700

    chore(pug): more pug

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.route("/").get((req, res) => {
   res.sendFile(process.cwd() + "views/pug/index.pug");
 });
 
+app.set("view engine", "pug");
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
 });
-
-app.set("view engine", "pug");
