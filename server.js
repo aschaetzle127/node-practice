a4ee5fc4c6f9132bc3bf945a31afce893be69b9d
Lexi127 <aperalta127@gmail.com>
2019-11-16 13:14:56 -0700

    feat(final): working version

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 "use strict";
 
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/public", express.static(path.join(process.cwd(), "/public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  res.sendFile(process.cwd() + "views/pug/index.pug");
-});
-
 app.set("view engine", "pug");
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+app.route("/").get((req, res) => {
+  res.render(path.join(process.cwd(), "/views/pug/index.pug"));
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port " + 3000);
 });
